fc9bbc0eb0bfd6ca5deed932f1a906cf6d2ec260
AlexisMotet <alexis.motet@ensta-bretagne.org>
2023-01-30 20:07:35 +0100

    ajout test attack

--- unit_tests.py
+++ unit_tests.py
@@ -2,13 +2,9 @@ import unittest
 import new_patch
 import constants.constants as c
 import time
-import numpy as np
 import torch
 import matplotlib.pyplot as plt
-import printability.new_printability as p
 import utils.utils as u
-import sklearn.cluster
-import PIL
 import torchvision
 import random
 import image_transformation.image_transformation as img_transfo
@@ -144,14 +140,10 @@ class ImageTransformation(unittest.TestCase):
             plt.pause(1)
         plt.show()
 
-        
 class Trainer(unittest.TestCase):
     def setUp(self):
         self.patch_trainer = new_patch.PatchTrainer(patch_relative_size=0.1)
                                                     
-        self.patch_trainer_flee = new_patch.PatchTrainer(mode=c.Mode.FLEE,
-                                                         threshold=0,
-                                                         patch_relative_size=0.05)
     def test_transformation(self):
         _, (ax1, ax2) = plt.subplots(1, 2)
         plt.suptitle("TEST TRANSFORMATION")
@@ -162,7 +154,45 @@ class Trainer(unittest.TestCase):
             ax2.imshow(u.tensor_to_array(transformed))
             plt.pause(0.5)
             
-    
+    def test_attack(self):
+        _, (ax1, ax2, ax3, ax4) = plt.subplots(1, 4)
+        plt.suptitle("TEST ATTACK")
+        trainer = self.patch_trainer
+        for img, true_label in trainer.train_loader:
+            vector_scores = trainer.model(trainer.extrinsic_module(img))
+            model_label = int(torch.argmax(vector_scores))
+            if model_label != int(true_label) :
+                continue
+            elif model_label == trainer.target_class:
+                continue
+            trainer.extrinsic_module.jitter()
+            trainer.intrinsic_module.jitter()
+            transformed, map_ = trainer.transformation_tool.random_transform(trainer.patch)
+            mask =  trainer._get_mask(transformed)
+            transformed.requires_grad = True
+            for i in range(trainer.max_iterations + 1) :
+                torch.clamp(transformed, 0, 1)
+                modified = trainer.intrinsic_module(transformed)
+                attacked = torch.mul(1 - mask, img) + \
+                        torch.mul(mask, modified)
+                normalized = trainer.extrinsic_module(attacked)
+                vector_scores = trainer.model(normalized)
+                vector_proba = torch.nn.functional.softmax(vector_scores, dim=1)
+                target_proba = float(vector_proba[0, trainer.target_class])
+                loss = -torch.nn.functional.log_softmax(vector_scores, dim=1)
+                loss[0, trainer.target_class].backward()
+                with torch.no_grad():
+                    transformed -= transformed.grad
+                transformed.grad.zero_()
+                print(target_proba)
+            ax1.imshow(u.tensor_to_array(img))
+            ax2.imshow(u.tensor_to_array(transformed))
+            ax3.imshow(u.tensor_to_array(modified))
+            ax4.imshow(u.tensor_to_array(attacked))
+            self.patch = trainer.transformation_tool.undo_transform(trainer.patch, 
+                                                            transformed.detach(),
+                                                            map_)
+            plt.pause(0.1)
         
     
 
