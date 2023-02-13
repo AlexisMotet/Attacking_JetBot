fb1312c1082cb3595874cd51bd9f5ac9fbdd9a87
AlexisMotet <84445302+AlexisMotet@users.noreply.github.com>
2023-02-13 15:37:48 +0100

    pr colab2

--- new_generate_adversarial_patch.py
+++ new_generate_adversarial_patch.py
@@ -1,8 +1,8 @@
 import new_patch
 import constants.constants as c
-from configs import config_aws
+from configs import config_colab
 
-patch_trainer = new_patch.PatchTrainer(config=config_aws, 
+patch_trainer = new_patch.PatchTrainer(config=config_colab, 
                                        mode=c.Mode.TARGET_AND_FLEE, n_epochs=3)
 patch_trainer.train()
 patch_trainer.save_patch("test.patch")
