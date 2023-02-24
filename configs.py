559d2eabbfbfe05b70ce8908277d1d5e467f3ee1
AlexisMotet <alexis.motet@ensta-bretagne.org>
2023-02-24 01:06:35 +0100

    config vdk

--- configs.py
+++ configs.py
@@ -87,5 +87,46 @@ config["PATH_DATASET"] = "C:\\Users\\alexi\\PROJET_3A\\imagenette2-160\\train\\"
 config["N_CLASSES"] = 10
 """
 
+config_VDK = {
+    "PATH_MODEL" : "C:/Users/alexi/PROJET_3A/projet_NOUINOU/fruit_model.pth",
+    "N_CLASSES" : 5,
+    "PATH_DATASET" : "C:/Users/alexi/PROJET_3A/fruits_resize",
+    "PATH_IMG_FOLDER" : path +  "projet_MOTET/images/",
+    "PATH_PRINTABLE_COLORS" : "printability/pantone-colors.json",
+    "BATCH_SIZE_TRAIN" : 10,
+    "BATCH_SIZE_TEST" : 1,
+    "LAMBDA_TV" : 0.003,
+    "LAMBDA_PRINT" : 0.002,
+    "THRESHOLD" : 0.9,
+    "MAX_ITERATIONS" : 10,
+    "FX" : 330.7111,
+    "FY" : 330.2779,
+    "CX" : 109.1424,
+    "CY" : 108.8565,
+    "K1" : -0.0973,
+    "K2" : 0.1732,
+    "EXTRINSIC_R" : np.eye(4),
+    "ANGLES_RANGE" : math.radians(5),
+    "SCALE_FACTOR_MIN" : 0.9,
+    "X_TOP_LEFT" : 40,
+    "Y_TOP_LEFT" : 40,
+    "X_BOTTOM_RIGHT" : 184,
+    "Y_BOTTOM_RIGHT" : 184,
+    "NOISE_STD" : np.array([0., 0., 0.]),
+    "BLUR_KERNEL_SIZE" : 5,
+    "BLUR_SIGMA_MAX" : 0.,
+    "BRIGHTNESS_BIAS" : 0.1,
+    "CONTRAST_GAIN" : 0.15,
+    "RESIZE_DIM" : 256,
+    "IMAGE_DIM" : 224,
+    "RATIO_TRAIN_TEST" : 3/4,
+    "NORMALIZATION_MEAN" : [0.485, 0.456, 0.406],
+    "NORMALIZATION_STD" : [0.229, 0.224, 0.225],
+    "LIMIT_TRAIN_EPOCH_LEN" : -1,
+    "LIMIT_TEST_LEN" : -1,
+    "N_ENREG_IMG" : 1,
+    "N_COLORS" : 100,
+}
+
 
 
